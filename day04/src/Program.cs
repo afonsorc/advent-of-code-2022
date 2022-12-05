78aa610a0af17c4e8c68a82944789c3b8e4d1611
afonsor97 <afonso-carvalho@hotmail.com>
2022-12-05 12:48:10 +0000

    feat: day04 completed

---
+++ day04/src/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace day04 {
+    class Program {
+        static void Main(string[] args){
+
+            // read and process input onto array
+            const string INPUT_FILE = "../input/input.txt";
+            string[] input = System.IO.File.ReadAllLines(INPUT_FILE);
+            
+            //partOne(input);   
+            partTwo(input);
+        }
+
+
+        static void partOne(string[] input){
+            
+            int contained = 0;
+            foreach(string row in input){
+
+                string[] pair = row.Split(',','-');
+                int[] range = Array.ConvertAll(pair, s => int.Parse(s));
+
+                if((range[2] <= range[0] && range[3] >= range[1]) || (range[0] <= range[2] && range[1] >= range[3])){
+                    contained++;
+                }
+            }
+
+            System.Console.WriteLine(contained);
+            return;
+        }
+
+
+        static void partTwo(string[] input){
+            
+            int contained = 0;
+            foreach(string row in input){
+
+                string[] pair = row.Split(',','-');
+                int[] range = Array.ConvertAll(pair, s => int.Parse(s));
+
+                if((range[0] > range[3] || range[1] < range[2])){
+                    continue;
+                }else{
+                    contained++;
+                }
+            }
+
+            System.Console.WriteLine(contained);
+            return;
+        }
+    }
+}
