0081034c07b6914c18059b70c2fe98e791d074dc
afonsor97 <afonso-carvalho@hotmail.com>
2022-12-05 16:56:49 +0000

    feat: day05 completed

--- new_day.sh
+++ new_day.sh
@@ -5,4 +5,14 @@ mkdir -p $DAY
 
 # copy contents
 cp -R base/. $DAY/
+
+
+# rename
 cd $DAY/src
+PROJ_FILE="base.csproj"
+NEW_PROJ_FILE="$DAY.csproj"
+
+if [ -f $PROJ_FILE ];
+then
+    mv "$PROJ_FILE" "$NEW_PROJ_FILE"
+fi
