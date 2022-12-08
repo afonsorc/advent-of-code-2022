693d16f961e541ffdad584ce86da521d5101b0df
afonsor97 <afonso-carvalho@hotmail.com>
2022-12-08 23:49:40 +0000

    fix: add user agent to input request

--- new_day.sh
+++ new_day.sh
@@ -1,5 +1,6 @@
 
 SESSION="53616c7465645f5f6a50d3d733d4b46eb55137cb3ddfc2b8dbfc5111085693f5ca179519be94e39304bc8e636824463565ec035ee1404f6965be803a1c5ffdc9"
+USER_AGENT = "github.com/afonsorc/advent-of-code-2022 by afonsorc@outlook.com"
 
 YEAR="2022"
 DAY=$1
@@ -25,7 +26,7 @@ cp -R base/. $DAY_FOLDER/
 
 # get input file
 cd $DAY_FOLDER/input
-curl https://adventofcode.com/$YEAR/day/$DAY/input --cookie "session=$SESSION" > input.txt
+curl -A $USER_AGENT https://adventofcode.com/$YEAR/day/$DAY/input --cookie "session=$SESSION" > input.txt
 
 
 # rename
