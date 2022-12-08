ae01f8cca8f7b179e5d58ecfa17bbeb821ad1d57
afonsor97 <afonso-carvalho@hotmail.com>
2022-12-08 23:30:16 +0000

    feat: automate input download

--- new_day.sh
+++ new_day.sh
@@ -1,16 +1,37 @@
+
+SESSION="53616c7465645f5f6a50d3d733d4b46eb55137cb3ddfc2b8dbfc5111085693f5ca179519be94e39304bc8e636824463565ec035ee1404f6965be803a1c5ffdc9"
+
+YEAR="2022"
+DAY=$1
+DAY_FIXED=$DAY
+
+# fix day variable
+LEN=`echo -n $DAY | wc -c`
+if [ $LEN == 1 ];
+then
+    DAY_FIXED="0$DAY"
+fi
+
+DAY_FOLDER="day$DAY_FIXED"
+
+
 # new dir for day
-DAY="day$1"
-mkdir -p $DAY
+mkdir -p $DAY_FOLDER
 
 
 # copy contents
-cp -R base/. $DAY/
+cp -R base/. $DAY_FOLDER/
+
+
+# get input file
+cd $DAY_FOLDER/input
+curl https://adventofcode.com/$YEAR/day/$DAY/input --cookie "session=$SESSION" > input.txt
 
 
 # rename
-cd $DAY/src
+cd ../src
 PROJ_FILE="base.csproj"
-NEW_PROJ_FILE="$DAY.csproj"
+NEW_PROJ_FILE="$DAY_FOLDER.csproj"
 
 if [ -f $PROJ_FILE ];
 then
