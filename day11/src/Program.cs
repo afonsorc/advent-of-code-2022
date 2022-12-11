e5e79ada4a3ea2e4d875c5b14042838392de921a
afonsor97 <afonso-carvalho@hotmail.com>
2022-12-11 23:41:41 +0000

    refactor: day11 cleanup

--- day11/src/Program.cs
+++ day11/src/Program.cs
@@ -4,7 +4,23 @@ using System.Linq;
 
 
 namespace day11{
-    class Program {
+    class Monkey{
+
+        public List<long> items;
+        public char operation;
+        public string operand;
+        public int test;
+        public int T;
+        public int F;
+        public long inspections;            
+
+        public Monkey(){
+            items = new List<long>();
+        }
+    }
+
+
+    class Program{
         static void Main(string[] args){
 
             // read and process input onto array
@@ -16,245 +32,95 @@ namespace day11{
             return;
         }
 
-        public struct Monkey{
 
-            public int monkey;
-            public List<ulong> items;
-            public char op;
-            public string opVal;
-            public int test;
-            public int testT;
-            public int testF;
-            public ulong inspections;            
-
-            public void newMonkey(int m){
-                monkey = m;
-                items = new List<ulong>();
-            }
-
-        }
-
-        static ulong partOne(string[] input){
-            
-            int nMonkeys = 0;
-            string[] line;
-
-            foreach(string i in input){
-                line = i.Split(' ',',');
-                if(line[0] == "Monkey"){
-                    nMonkeys++;
-                }
-                else if(line[0] == "Starting"){
-                    for(int j = 2; j < line.GetLength(0); j++){
-                        
-                    }
-                }
-            }
-            string[] charsToDelete = new string[]{":", ",", "  ", "    "};
-            Monkey[] monkeys = new Monkey[nMonkeys];
-            int curMonkey = -1;
-            foreach(string i in input){
-                string x = i;
-                foreach(string c in charsToDelete){
-                    x = x.Replace(c, string.Empty);
-                }
-
-                line = x.Split(' ');
-                if(line[0] == "Monkey"){
-                    curMonkey++;
-                    monkeys[curMonkey].newMonkey(curMonkey);
-                }
-                else if(line[0] == "Starting"){
-                    for(int j = 2; j < line.GetLength(0); j++){
-                        monkeys[curMonkey].items.Add(Convert.ToUInt64(line[j]));
-                    }
-                }
-                else if(line[0] == "Operation"){
-                    monkeys[curMonkey].op = char.Parse(line[4]);
-                    monkeys[curMonkey].opVal = line[5];
-                }
-                else if(line[0] == "Test"){
-                    monkeys[curMonkey].test = Convert.ToInt32(line[3]);
-                }
-                else if(line[0] == "If"){
-                    if(line[1] == "true") monkeys[curMonkey].testT = Convert.ToInt32(line[5]);
-                    else if(line[1] == "false") monkeys[curMonkey].testF = Convert.ToInt32(line[5]);
-                }
-            }
-
-
-            for(int r = 0; r < 20; r++){
-                for(int m = 0; m < nMonkeys; m++){
-                    foreach(ulong item in monkeys[m].items){
-                        ulong cur = item;
-                        
-                        if(monkeys[m].op == '*'){
-                            if(monkeys[m].opVal == "old"){
-                                cur *= cur;
-                            } else{
-                                cur *= Convert.ToUInt64(monkeys[m].opVal);
-                            }
-                        }else if(monkeys[m].op == '+'){
-                            if(monkeys[m].opVal == "old"){
-                                cur += cur;
-                            } else{
-                                cur += Convert.ToUInt64(monkeys[m].opVal);
-                            }
-                        }
-
-                        cur /= 3;
-
-                        if(cur % Convert.ToUInt64(monkeys[m].test) == 0){
-                            monkeys[monkeys[m].testT].items.Add(cur);
-                        }else{
-                            monkeys[monkeys[m].testF].items.Add(cur);
-                        }
-                        monkeys[m].inspections++;
-                    }
-                    monkeys[m].items.Clear();
-                }
-            }
-
-            ulong[] mostInsp = new ulong[2];
-
-
-            foreach(Monkey m in monkeys){
-
-                //    System.Console.WriteLine("-------------");
-                //foreach(int i in m.items) System.Console.Write("{0}-",i);
-                //System.Console.WriteLine();
-                //System.Console.WriteLine(m.op);
-                //System.Console.WriteLine(m.opVal);
-                //System.Console.WriteLine(m.test);
-                //System.Console.WriteLine(m.testT);
-                //System.Console.WriteLine(m.testF);
-                //System.Console.WriteLine(m.inspections);
-                //System.Console.WriteLine("-------------");
-
-                if(m.inspections > mostInsp.Min()) mostInsp[Array.IndexOf(mostInsp, mostInsp.Min())] = m.inspections;
-            }
-
-
-            System.Console.WriteLine(mostInsp[0]);
-                    System.Console.WriteLine(mostInsp[1]);
-            return mostInsp[0]*mostInsp[1];
+        static long partOne(string[] input){
+            return monkeyInTheMiddle(input, 20, false);
         }
 
 
-        static ulong partTwo(string[] input){
-            
-            int nMonkeys = 0;
-            string[] line;
+        static long partTwo(string[] input){
+            return monkeyInTheMiddle(input, 10000, true);
+        }
 
+
+        static long monkeyInTheMiddle(string[] input, int rounds, bool isVeryWorried){
+
+            long monkeyBusiness = 0;
+            int numberOfMonkeys = 0;
+
+            // count number of monkeys
             foreach(string i in input){
-                line = i.Split(' ',',');
-                if(line[0] == "Monkey"){
-                    nMonkeys++;
-                }
-                else if(line[0] == "Starting"){
-                    for(int j = 2; j < line.GetLength(0); j++){
-                        
-                    }
-                }
+                string[] line = i.Split(' ');
+                if(line[0] == "Monkey") numberOfMonkeys++;
             }
-            string[] charsToDelete = new string[]{":", ",", "  ", "    "};
-            Monkey[] monkeys = new Monkey[nMonkeys];
-            int curMonkey = -1;
-            foreach(string i in input){
-                string x = i;
-                foreach(string c in charsToDelete){
-                    x = x.Replace(c, string.Empty);
-                }
 
-                line = x.Split(' ');
+            // init monkey array
+            string[] charsToDelete = new string[]{":", ",", "  ", "    "};
+            Monkey[] monkeys = new Monkey[numberOfMonkeys];
+            for(int i = 0; i < numberOfMonkeys; i++){
+                monkeys[i] = new Monkey();
+            } 
+
+            // process all inputs
+            int lcm = 1;
+            int curMonkey = -1;
+            for(int i = 0; i < input.Length; i++){
+                foreach(string c in charsToDelete){
+                    input[i] = input[i].Replace(c, string.Empty);
+                }
+                string[] line = input[i].Split(' ');
                 if(line[0] == "Monkey"){
                     curMonkey++;
-                    monkeys[curMonkey].newMonkey(curMonkey);
-                }
-                else if(line[0] == "Starting"){
+                }else if(line[0] == "Starting"){
                     for(int j = 2; j < line.GetLength(0); j++){
-                        monkeys[curMonkey].items.Add(Convert.ToUInt64(line[j]));
+                        monkeys[curMonkey].items.Add(Convert.ToInt64(line[j]));
                     }
-                }
-                else if(line[0] == "Operation"){
-                    monkeys[curMonkey].op = char.Parse(line[4]);
-                    monkeys[curMonkey].opVal = line[5];
-                }
-                else if(line[0] == "Test"){
+                }else if(line[0] == "Operation"){
+                    monkeys[curMonkey].operation = char.Parse(line[4]);
+                    monkeys[curMonkey].operand = line[5];
+                }else if(line[0] == "Test"){
                     monkeys[curMonkey].test = Convert.ToInt32(line[3]);
-                }
-                else if(line[0] == "If"){
-                    if(line[1] == "true") monkeys[curMonkey].testT = Convert.ToInt32(line[5]);
-                    else if(line[1] == "false") monkeys[curMonkey].testF = Convert.ToInt32(line[5]);
+                    lcm *= monkeys[curMonkey].test;
+                }else if(line[0] == "If"){
+                    if(line[1] == "true") monkeys[curMonkey].T = Convert.ToInt32(line[5]);
+                    else if(line[1] == "false") monkeys[curMonkey].F = Convert.ToInt32(line[5]);
                 }
             }
 
-            ulong lcm = 1;
-
-            foreach(Monkey m in monkeys){
-                lcm *= (ulong) m.test;
-            }
-            System.Console.WriteLine(lcm);
-
-            for(int r = 0; r < 10000; r++){
-                for(int m = 0; m < nMonkeys; m++){
-                    foreach(ulong item in monkeys[m].items){
-                        ulong cur = item;
+            // play out rounds
+            for(int r = 0; r < rounds; r++){
+                foreach(Monkey m in monkeys){
+                    foreach(long item in m.items){
                         
-                        if(monkeys[m].op == '*'){
-                            if(monkeys[m].opVal == "old"){
-                                cur *= cur;
-                            } else{
-                                cur *= Convert.ToUInt64(monkeys[m].opVal);
-                            }
-                        }else if(monkeys[m].op == '+'){
-                            if(monkeys[m].opVal == "old"){
-                                cur += cur;
-                            } else{
-                                cur += Convert.ToUInt64(monkeys[m].opVal);
-                            }
-                        }
-                        //System.Console.WriteLine("{0}:{1}", cur, cur%lcm);
-                        cur %= lcm;
+                        long curItem = item;
+                        long operand = 0;
+                        
+                        if(m.operand == "old") operand = curItem;
+                        else operand = Convert.ToInt64(m.operand);
 
+                        if(m.operation == '*') curItem *= operand;
+                        else if(m.operation == '+') curItem += operand;
+                        
+                        if(isVeryWorried) curItem %= lcm;
+                        else curItem /= 3;
 
-                        if(cur % Convert.ToUInt64(monkeys[m].test) == 0){
-                            monkeys[monkeys[m].testT].items.Add(cur);
-                        }else{
-                            monkeys[monkeys[m].testF].items.Add(cur);
-                        }
-                        monkeys[m].inspections++;
+                        if(curItem % m.test == 0) monkeys[m.T].items.Add(curItem);
+                        else monkeys[m.F].items.Add(curItem);
                     }
-                    monkeys[m].items.Clear();
+
+                    m.inspections += m.items.Count;
+                    m.items.Clear();
                 }
             }
 
-
-            ulong[] mostInsp = new ulong[2];
-
-            
+            // calculate inspections
+            long[] mostInspections = new long[2];
             foreach(Monkey m in monkeys){
-
-                //    System.Console.WriteLine("-------------");
-                foreach(int i in m.items) System.Console.Write("{0} | ",i);
-                //System.Console.WriteLine();
-                //System.Console.WriteLine(m.op);
-                //System.Console.WriteLine(m.opVal);
-                //System.Console.WriteLine(m.test);
-                //System.Console.WriteLine(m.testT);
-                //System.Console.WriteLine(m.testF);
-                System.Console.WriteLine(m.inspections);
-                //System.Console.WriteLine("-------------");
+                if(m.inspections > mostInspections.Min()) mostInspections[Array.IndexOf(mostInspections, mostInspections.Min())] = m.inspections;
             }
 
-            foreach(Monkey m in monkeys){
-
-                if(m.inspections > mostInsp.Min()) mostInsp[Array.IndexOf(mostInsp, mostInsp.Min())] = m.inspections;
-            }
-
-            System.Console.WriteLine(mostInsp[0]);
-                System.Console.WriteLine(mostInsp[1]);
-            return mostInsp[0]*mostInsp[1];
+            monkeyBusiness = mostInspections[0] * mostInspections[1];
+            return monkeyBusiness;
         }
     }
 }
