c14b7c3f774173115fe196a2e1287de01c84067a
afonsor97 <afonso-carvalho@hotmail.com>
2022-12-12 18:33:36 +0000

    refactor: day12 clean up

--- day12/src/Program.cs
+++ day12/src/Program.cs
@@ -17,200 +17,90 @@ namespace day12{
 
 
         static int partOne(string[] input){
-
-            int solution = 0;
-            int len = input[0].Length;
-            char[] grid = new char[len * input.Length];
-            int start = 0;
-            int end = 0;
-
-            for(int i = 0; i < input.Length; i++){
-                for(int j = 0; j < input[i].Length; j++){
-                    grid[i * len + j] = input[i][j];
-                    if(grid[i*len+j] == 'S'){
-                        start = i*len+j;
-                        grid[start] = 'a';
-                    }else if(grid[i*len+j] == 'E'){
-                        end = i*len+j;
-                        grid[end] = 'z';
-                    }
-                }
-            }
-
-            
-
-            // print line
-            for(int i = 0; i < input.Length; i++){
-                for(int j = 0; j < input[i].Length; j++){
-                    System.Console.Write(grid[i * len + j]);
-                }
-                System.Console.WriteLine();
-            }
-            System.Console.WriteLine();
-
-
-
-            int path = 0;
-            int[] visited = new int[len * input.Length];
-            for(int i = 0; i < visited.Length; i++){
-                visited[i]=-1;
-            }
-
-
-            Queue<int> queue = new Queue<int>();
-            queue.Enqueue(start);
-            visited[start] = start;
-
-            while (queue.Count > 0){
-                
-                int element = queue.Dequeue();
-                //System.Console.WriteLine(element);
-                if(element == end)
-                    break;
-
-            
-                
-                List<int> neighbours = new List<int>();
-
-
-                if(element % len != 0){
-                    int climb = grid[element - 1] - grid[element];
-                    if(climb < 2) neighbours.Add(element - 1);
-                }
-                if(element % len != len - 1){
-                    int climb = grid[element + 1] - grid[element];
-                    if(climb < 2) neighbours.Add(element + 1);
-                }
-                if(element - len >= 0){
-                    int climb = grid[element - len] - grid[element];
-                    if(climb < 2) neighbours.Add(element - len);
-                }
-                if(element + len < len * input.Length){
-                    int climb = grid[element + len] - grid[element];
-                    if(climb < 2) neighbours.Add(element + len);
-                }
-                //foreach(int n in neighbours) System.Console.Write("{0}|", n);
-                //System.Console.WriteLine();
-                if (neighbours == null)
-                    continue;
-                foreach (int n in neighbours)
-                {
-                    if(visited[n] == -1){
-                        queue.Enqueue(n);
-                        visited[n] = element;
-                    }
-                }
-                neighbours.Clear();
-            }
-
-            int cur = end;
-            while(cur != start){
-                System.Console.WriteLine(cur);
-                cur = visited[cur];
-                path++;
-            }
-
-            return path;
+            return hillClimbingAlgorithm(input, false);
         }
 
 
         static int partTwo(string[] input){
-            
-            int solution = 0;
-            int len = input[0].Length;
-            char[] grid = new char[len * input.Length];
+            return hillClimbingAlgorithm(input, true);
+        }
+
+
+        static int hillClimbingAlgorithm(string[] input, bool isStartingSpotFixed){
+            int width = input[0].Length;
+            char[] grid = new char[width * input.Length];
             int start = 0;
             int end = 0;
 
+            // process input
             for(int i = 0; i < input.Length; i++){
                 for(int j = 0; j < input[i].Length; j++){
-                    grid[i * len + j] = input[i][j];
-                    if(grid[i*len+j] == 'S'){
-                        start = i*len+j;
+                    grid[i * width + j] = input[i][j];
+                    if(grid[i * width + j] == 'S'){
+                        start = i * width + j;
                         grid[start] = 'a';
-                    }else if(grid[i*len+j] == 'E'){
-                        end = i*len+j;
+                    }else if(grid[i * width + j] == 'E'){
+                        end = i * width + j;
                         grid[end] = 'z';
                     }
                 }
             }
 
-            
-
-            // print line
-            for(int i = 0; i < input.Length; i++){
-                for(int j = 0; j < input[i].Length; j++){
-                    System.Console.Write(grid[i * len + j]);
-                }
-                System.Console.WriteLine();
-            }
-            System.Console.WriteLine();
-
-
-
-            int path = 0;
-            int[] visited = new int[len * input.Length];
+            // init visited array
+            int[] visited = new int[width * input.Length];
             for(int i = 0; i < visited.Length; i++){
-                visited[i]=-1;
+                visited[i]= -1;
             }
 
-
+            // bfs
+            int[] step = new int[4]{1, -1, -width, width};
             Queue<int> queue = new Queue<int>();
             queue.Enqueue(end);
             visited[end] = end;
-
             while (queue.Count > 0){
                 
-                int element = queue.Dequeue();
-                System.Console.WriteLine(element);
-                System.Console.WriteLine(grid[element]);
-                if(grid[element] == 'a'){
-                    start = element;
+                int spot = queue.Dequeue();
+                if(spot == start && !isStartingSpotFixed){
+                    break;
+                }else if(grid[spot] == 'a' && isStartingSpotFixed){
+                    start = spot;
                     break;
                 }
 
-
-                
                 List<int> neighbours = new List<int>();
 
-                if(element % len != 0){
-                    int climb = grid[element - 1] - grid[element];
-                    if(climb > -2) neighbours.Add(element - 1);
+                if(spot % width != width - 1){
+                    if(grid[spot] - grid[spot + step[0]] < 2) neighbours.Add(spot + step[0]);
                 }
-                if(element % len != len - 1){
-                    int climb = grid[element + 1] - grid[element];
-                    if(climb > -2) neighbours.Add(element + 1);
+                if(spot % width != 0){
+                    if(grid[spot] - grid[spot + step[1]] < 2) neighbours.Add(spot + step[1]);
                 }
-                if(element - len >= 0){
-                    int climb = grid[element - len] - grid[element];
-                    if(climb > -2) neighbours.Add(element - len);
+                if(spot >= width){
+                    if(grid[spot] - grid[spot + step[2]] < 2) neighbours.Add(spot + step[2]);
                 }
-                if(element + len < len * input.Length){
-                    int climb = grid[element + len] - grid[element];
-                    if(climb > -2) neighbours.Add(element + len);
+                if(spot < width * input.Length - width){
+                    if(grid[spot] - grid[spot + step[3]] < 2) neighbours.Add(spot + step[3]);
                 }
-                foreach(int n in neighbours) System.Console.Write("{0}|", n);
-                System.Console.WriteLine();
-                if (neighbours == null)
-                    continue;
-                foreach (int n in neighbours)
-                {
+
+                foreach (int n in neighbours){
                     if(visited[n] == -1){
                         queue.Enqueue(n);
-                        visited[n] = element;
+                        visited[n] = spot;
                     }
                 }
+
                 neighbours.Clear();
             }
 
-            int cur = start;
-            while(cur != end){
-                //System.Console.WriteLine(cur);
-                cur = visited[cur];
-                path++;
+            /// get shortest path
+            int shortestPath = 0;
+            int current = start;
+            while(current != end){
+                current = visited[current];
+                shortestPath++;
             }
 
-            return path;
+            return shortestPath;
         }
     }
 }
